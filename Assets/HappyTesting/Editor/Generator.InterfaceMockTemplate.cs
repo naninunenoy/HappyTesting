e31d3e2affd90b906fc9d5f6600f9f3613df282c
naninunenoy <naninuneno0411@gmail.com>
2022-01-28 19:36:45 +0900

    setterのmock自動生成

--- Assets/HappyTesting/Editor/Generator.InterfaceMockTemplate.cs
+++ Assets/HappyTesting/Editor/Generator.InterfaceMockTemplate.cs
@@ -1,34 +1,60 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Text;
 
 namespace HappyTesting.Editor {
     internal static partial class Generator {
-        const string interfaceMockGenericsTypeName = "<#=TYPE_NAME#>";
-        const string interfaceMockTemplateIReadonlyReactivePropertyTag = "<#=IREADONLYREACTIVEPROPERTY_NAME#>";
-        const string interfaceMockTemplateSetterTag = "<#=SETTER_NAME#>";
-        const string interfaceMockTemplateUniTaskTag = "<#=UNITASK_NAME#>";
-        const string interfaceMockTemplateTaskTag = "<#=TASK_NAME#>";
-
         internal static string GetObservablePair(string typeName, string observableName) {
-            var subjectName = $"{observableName}Subject";
+            var subjectName = $"{observableName.ToPascalCase()}Subject";
             return $"public IObservable<{typeName}> {observableName} => {subjectName};\n" +
                    $"public readonly Subject<{typeName}> {subjectName} = new();";
         }
 
+        internal static string GetIReadonlyReactivePropertyPair(string typeName, string iReadOnlyReactivePropertyName) {
+            var subjectName = $"{iReadOnlyReactivePropertyName.ToPascalCase()}Subject";
+            return $"public IReadonlyReactiveProperty<{typeName}> {iReadOnlyReactivePropertyName} => {subjectName}.ToReadonlyReactiveProperty();\n" +
+                   $"public readonly Subject<{typeName}> {subjectName} = new();";
+        }
+
+        internal static string GetResultName(string setterName, string argName) {
+            return $"{setterName}{argName.ToPascalCase()}Result";
+        }
+
+        internal static string GetSetterArgs(string setterName, (string typeName, string argName)[] paramList) {
+            return $"public void {setterName}(" +
+                   $"{paramList.Select(x => $"{x.typeName} {x.argName}").Aggregate((prev, total) => $"{prev}, {total}")}" +
+                   $")";
+        }
+
+        internal static string GetSetterBody(string setterName, (string typeName, string argName)[] paramList) {
+            return paramList.Select(x => new { result = GetResultName(setterName, x.argName), x.argName })
+                .Select(x => $"  {x.result} = {x.argName};")
+                .Aggregate((prev, total) => $"{prev}\n{total}");
+        }
+
+        internal static string GetSetterResultList(string setterName, (string typeName, string argName)[] paramList) {
+            return paramList
+                .Select(x =>
+                    $"public {x.typeName} {GetResultName(setterName, x.argName)} {{ private set; get; }}")
+                .Aggregate((prev, total) => $"{prev}\n{total}");
+        }
+
         internal static string GetSetterPair(string setterName, (string typeName, string argName)[] paramList) {
             StringBuilder stringBuilder = new();
-            var args = paramList.Select(x => $"{x.typeName} {x.argName}").Aggregate((a, b) => $"{a}, {b}");
-            foreach (var (typeName, argName) in paramList) {
-                var resultName = $"{setterName}{argName}Result";
-                var setter = $"public void {setterName}({args}) {{" +
-                             $"}}";
-            }
-
-            string getResultStr(string typeName, string argName) {
-                return $"public {typeName} {setterName}{argName}Result;";
-            }
-
+            stringBuilder.Append($"{GetSetterResultList(setterName, paramList)}\n");
+            stringBuilder.Append($"{GetSetterArgs(setterName, paramList)} {{\n");
+            stringBuilder.Append($"{GetSetterBody(setterName, paramList)}\n");
+            stringBuilder.Append($"}}");
             return stringBuilder.ToString();
+        }
+
+        internal static string ToPascalCase(this string x) {
+            return char.ToUpperInvariant(x[0]) + x[1..];
+        }
+
+        internal static string ToCamelCase(this string x) {
+            return char.ToLowerInvariant(x[0]) + x[1..];
+
         }
     }
 }
--- Assets/HappyTesting/Editor/Tests/InterfaceMockTemplateTest.cs
+++ Assets/HappyTesting/Editor/Tests/InterfaceMockTemplateTest.cs
@@ -4,14 +4,45 @@ using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
 
-namespace HappyTesting.Editor.Tests
-{
-    public class InterfaceMockTemplateTest
-    {
+namespace HappyTesting.Editor.Tests {
+    public class InterfaceMockTemplateTest {
         [Test]
-        public void InterfaceMockTemplateTestSimplePasses()
-        {
-            Generator.GenerateTestTemplate();
+        public void GetObservablePairTest() {
+            var code = Generator.GetObservablePair("Unit", "Hoge");
+            Assert.That(code.Contains(@"public IObservable<Unit> Hoge => HogeSubject;"), Is.True, code);
+            Assert.That(code.Contains(@"public readonly Subject<Unit> HogeSubject = new();"), Is.True, code);
+        }
+        [Test]
+        public void GetIReadonlyReactivePropertyObservablePairTest() {
+            var code = Generator.GetIReadonlyReactivePropertyPair("int", "Fuga");
+            Assert.That(code.Contains(@"public IReadonlyReactiveProperty<int> Fuga => FugaSubject.ToReadonlyReactiveProperty();"), Is.True, code);
+            Assert.That(code.Contains(@"public readonly Subject<int> FugaSubject = new();"), Is.True, code);
+        }
+        [Test]
+        public void GetSetterResultListTest() {
+            var code = Generator.GetSetterResultList("SetHoge",
+                new[] { ("int", "val1"), ("float", "val2") });
+            Assert.That(code.Contains(@"public int SetHogeVal1Result { private set; get; }"), Is.True, code);
+            Assert.That(code.Contains(@"public float SetHogeVal2Result { private set; get; }"), Is.True, code);
+        }
+        [Test]
+        public void GetSetterArgsTest() {
+            var code = Generator.GetSetterArgs("SetHoge",
+                new[] { ("int", "val1"), ("float", "val2") });
+            Assert.That(code.Contains(@"public void SetHoge(int val1, float val2)"), Is.True, code);
+        }
+        [Test]
+        public void GetSetterBodyTest() {
+            var code = Generator.GetSetterBody("SetHoge",
+                new[] { ("int", "val1"), ("float", "val2") });
+            Assert.That(code.Contains(@"  SetHogeVal1Result = val1;"), Is.True, code);
+            Assert.That(code.Contains(@"  SetHogeVal2Result = val2;"), Is.True, code);
+        }
+        [Test]
+        public void GetSetterPairTest() {
+            var code = Generator.GetSetterPair("SetHoge",
+                new[] { ("int", "val1"), ("float", "val2") });
+            Debug.Log(code);
         }
     }
 }
