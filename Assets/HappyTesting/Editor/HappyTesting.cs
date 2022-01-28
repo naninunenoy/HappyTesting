50fcc11a91432d7f0e122940e65a578585299abc
naninunenoy <naninuneno0411@gmail.com>
2022-01-28 16:31:26 +0900

    add empty class

---
+++ Assets/HappyTesting/Editor/HappyTesting.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace HappyTesting.Editor
+{
+    public static partial class HappyTesting
+    {
+    
+    }
+}
