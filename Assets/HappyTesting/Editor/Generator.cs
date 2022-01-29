73a65ec598c15151f6831499d6c32ee8f3544048
naninunenoy <naninuneno0411@gmail.com>
2022-01-29 13:17:27 +0900

    コード解析を並列に行う

--- Assets/HappyTesting/Editor/Generator.cs
+++ Assets/HappyTesting/Editor/Generator.cs
@@ -1,10 +1,13 @@
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
 
 namespace HappyTesting.Editor {
     internal static partial class Generator {
+        static readonly object parallellLock = new();
         [MenuItem("Assets/HappyTesting/Generate TestCode Template")]
         public static void GenerateTestTemplate() {
             // Selectionがら対象取得
@@ -17,12 +20,18 @@ namespace HappyTesting.Editor {
                 return;
             }
             // コードの生成
-            foreach (var text in texts) {
-                var param = LoadEditModeTestGenerateParam(text.text);
-                var fullText = GetEditModeTestFullText(param);
-                GenerateScriptAsset(fullText, destDir, $"{param.testClassName}.cs");
-            }
-            AssetDatabase.Refresh();
+            var scripts = texts.Select(x => x.text).ToArray();
+            List<(string code, string fileName)> generated = new();
+            Parallel.ForEach(scripts, script => {
+                    var param = LoadEditModeTestGenerateParam(script);
+                    var fullText = GetEditModeTestFullText(param);
+                    lock (parallellLock) {
+                        generated.Add((fullText, $"{param.testClassName}.cs"));
+                    }
+                }
+            );
+            Debug.Log(string.Join(",", generated.Select(x => x.fileName)));
+            GenerateScriptAsset(destDir, generated);
         }
 
         [MenuItem("Assets/HappyTesting/Generate Interface TestMock")]
@@ -36,12 +45,17 @@ namespace HappyTesting.Editor {
                 return;
             }
             // コードの生成
-            foreach (var text in texts) {
-                var param = LoadInterfaceMockGenerateParam(text.text);
-                var fullText = GetInterfaceTestMockFullText(param);
-                GenerateScriptAsset(fullText, destDir, $"{param.className}.cs");
-            }
-            AssetDatabase.Refresh();
+            var scripts = texts.Select(x => x.text).ToArray();
+            List<(string code, string fileName)> generated = new();
+            Parallel.ForEach(scripts, script => {
+                    var param = LoadInterfaceMockGenerateParam(script);
+                    var fullText = GetInterfaceTestMockFullText(param);
+                    lock (parallellLock) {
+                        generated.Add((fullText, $"{param.className}.cs"));
+                    }
+                }
+            );
+            GenerateScriptAsset(destDir, generated);
         }
 
         static bool ContainsScriptInSelection(out TextAsset[] scripts) {
@@ -62,9 +76,12 @@ namespace HappyTesting.Editor {
             return EditorUtility.SaveFolderPanel("select destination", Application.dataPath, "");
         }
 
-        static void GenerateScriptAsset(string content, string saveTo, string fileName) {
-            var saveFullPath = Path.Combine(string.IsNullOrEmpty(saveTo) ? "Assets" : saveTo, fileName);
-            File.WriteAllText(saveFullPath, content);
+        static void GenerateScriptAsset(string saveTo, List<(string code, string fileName)> generated) {
+            foreach (var (code, fileName) in generated) {
+                var saveFullPath = Path.Combine(saveTo, fileName);
+                File.WriteAllText(saveFullPath, code);
+            }
+            AssetDatabase.Refresh();
         }
     }
 }
